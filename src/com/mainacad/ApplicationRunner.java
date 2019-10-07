16ed7458378b9bc16dcf6f12fffcb790260cadd4
AlexK-Com <alexk.com@gmail.com>
2019-10-07 19:31:42 +0300

    initial commit

---
+++ src/com/mainacad/ApplicationRunner.java
@@ -0,0 +1,33 @@
+package com.mainacad;
+
+import com.mainacad.circle.circle;
+import com.mainacad.square.Square;
+import com.mainacad.triangle.triangle;
+
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        Square square = new Square();
+        square.setSide(10.0);
+
+        circle circle = new circle();
+        circle.setRadius(10.0);
+
+        triangle triangle = new triangle();
+        triangle.setSide1(3.0);
+        triangle.setSide2(4.0);
+        triangle.setSide3(5.0);
+
+        System.out.println(
+              "Square is: " + square.getArea()
+        );
+        System.out.println(
+                "circle is: " + circle.getArea()
+        );
+        System.out.println(
+                "Triangle is: " + triangle.getArea()
+        );
+
+    }
+
+
+}
---
+++ src/com/mainacad/circle/circle.java
@@ -0,0 +1,19 @@
+package com.mainacad.circle;
+
+public class circle {
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+
+}
